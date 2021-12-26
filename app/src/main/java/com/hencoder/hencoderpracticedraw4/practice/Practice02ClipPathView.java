e2804e7d0d6a9a1f8a2bc688a5c6645dd6810d86
luyao <luyao@douyu.tv>
2021-12-26 15:09:30 +0800

    【自定义View1-4】作业1,2提交

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
@@ -7,7 +7,9 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -35,11 +37,25 @@ public class Practice02ClipPathView extends View {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        // canvas.save();与canvas.restore();一般结合使用，.save()函数在前，.restore()函数在后，
+        // 用来保证在这两个函数之间所做的操作不会对原来在canvas上所画图形产生影响
 
+        canvas.save();
+        Path path1 = new Path();
+        path1.addCircle(point1.x + 200, point1.y + 200, 150, Path.Direction.CW);
+        canvas.clipPath(path1);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        Path path2 = new Path();
+        path2.addCircle(point2.x + 200, point2.y + 200, 150, Path.Direction.CW);
+        canvas.clipOutPath(path2);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
