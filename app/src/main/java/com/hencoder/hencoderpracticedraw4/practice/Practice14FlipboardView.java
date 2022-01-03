6e834a7d53d925bef54b8597a255f0bad6fce87e
luyao <luyao@douyu.tv>
2022-01-03 09:53:49 +0800

    【自定义View1-4】作业14提交

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
@@ -10,6 +10,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 
@@ -19,7 +20,7 @@ public class Practice14FlipboardView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
     Camera camera = new Camera();
-    int degree;
+    int degree = 0;
     ObjectAnimator animator = ObjectAnimator.ofInt(this, "degree", 0, 180);
 
     public Practice14FlipboardView(Context context) {
@@ -55,16 +56,21 @@ public class Practice14FlipboardView extends View {
         animator.end();
     }
 
+    /**
+     * 由于此方法在执行属性动画时，会不断的设置degree属性，导致invalidate->onDraw，达到旋转的效果。
+     */
     @SuppressWarnings("unused")
     public void setDegree(int degree) {
         this.degree = degree;
         invalidate();
+        Log.e("luy", "setDegree, degree : "+degree);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        Log.e("luy", "onDraw, degree : "+degree);
         int bitmapWidth = bitmap.getWidth();
         int bitmapHeight = bitmap.getHeight();
         int centerX = getWidth() / 2;
@@ -72,12 +78,20 @@ public class Practice14FlipboardView extends View {
         int x = centerX - bitmapWidth / 2;
         int y = centerY - bitmapHeight / 2;
 
+        // 上半部分
+        canvas.save();
+        canvas.clipRect(x, y, x + bitmapWidth, y + bitmapHeight / 2);
+        canvas.drawBitmap(bitmap, x, y, paint);
+        canvas.restore();
+
+        // 下半部分
         canvas.save();
 
         camera.save();
         camera.rotateX(degree);
         canvas.translate(centerX, centerY);
         camera.applyToCanvas(canvas);
+        canvas.clipRect(-bitmapWidth / 2, 0, bitmapWidth / 2, bitmapHeight / 2);
         canvas.translate(-centerX, -centerY);
         camera.restore();
 
